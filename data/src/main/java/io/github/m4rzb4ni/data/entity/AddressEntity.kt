59dbc7b7651bc16deff010af93020b18ab7f9d03
Hamid Marzbani <hamidreza.marzbani@nexttoptech.org>
2026-01-04 20:09:24 +0330

    refactor: flatten address entity in property table

---
+++ data/src/main/java/io/github/m4rzb4ni/data/entity/AddressEntity.kt
@@ -0,0 +1,9 @@
+package io.github.m4rzb4ni.data.entity
+
+data class AddressEntity(
+    val street: String,
+    val city: String,
+    val state: String,
+    val zipCode: String,
+    val country: String
+)
--- data/src/main/java/io/github/m4rzb4ni/data/entity/PropertyEntity.kt
+++ data/src/main/java/io/github/m4rzb4ni/data/entity/PropertyEntity.kt
@@ -1,5 +1,6 @@
 package io.github.m4rzb4ni.data.entity
 
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.time.LocalDate
@@ -8,7 +9,7 @@ import java.time.LocalDate
 data class PropertyEntity(
     @PrimaryKey val id: String,
     val name: String,
-    val address: String,
+    @Embedded val address: AddressEntity,
     val type: String,
     val sizeInSqm: Float,
     val numberOfRooms: Int,
