b9d231689e90a4b02404b5abb175a2920bd30573
Hamid Marzbani <hamidreza.marzbani@nexttoptech.org>
2026-01-04 20:09:45 +0330

    feat: write mapper between entity and model

--- data/src/main/java/io/github/m4rzb4ni/data/repository/LocalPropertyRepository.kt
+++ data/src/main/java/io/github/m4rzb4ni/data/repository/LocalPropertyRepository.kt
@@ -1,27 +1,27 @@
 package io.github.m4rzb4ni.data.repository
 
 import io.github.m4rzb4ni.data.dao.PropertyDao
-import io.github.m4rzb4ni.domain.enumerations.PropertyType
-import io.github.m4rzb4ni.domain.model.Address
+import io.github.m4rzb4ni.data.mapper.toDomain
 import io.github.m4rzb4ni.domain.model.Property
 import io.github.m4rzb4ni.domain.repository.PropertyRepository
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class LocalPropertyRepository @Inject constructor(
-    private val dao: PropertyDao
+@Singleton
+class LocalPropertyRepository @Inject constructor(private val dao: PropertyDao) :
+    PropertyRepository {
+    override suspend fun getProperties(): Result<List<Property>> {
+        return runCatching {
+            val entities = dao.getAllProperties()
+            if (entities.isEmpty()) throw Exception("No properties found")
+            entities.map { entity -> entity.toDomain() }
+        }
+    }
 
-) : PropertyRepository {
-    override suspend fun getProperties(): List<Property> {
-        return dao.getAllProperties()
-            .map { entity ->
-                Property(
-                    id = entity.id,
-                    name = entity.name,
-                    address = Address(entity.address, "", "", "", ""),
-                    type = PropertyType.valueOf(entity.type),
-                    sizeInSqm = entity.sizeInSqm,
-                    numberOfRooms = entity.numberOfRooms,
-                )
-            }
+    override suspend fun getPropertyById(id: String): Result<Property> {
+        return runCatching {
+            val entity = dao.getPropertyById(id = id) ?: throw Exception("No Property was found")
+            entity.toDomain()
+        }
     }
 }
