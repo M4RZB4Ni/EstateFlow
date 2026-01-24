88fea18cf06ea6a3400efd57489a2271b172f56e
Hamid Marzbani <hamidreza.marzbani@nexttoptech.org>
2026-01-24 15:10:13 +0300

    feat: Add NavHost and update MainActivity

---
+++ app/src/main/java/io/github/m4rzb4ni/estateflow/MainActivity.kt
@@ -0,0 +1,17 @@
+package io.github.m4rzb4ni.estateflow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EstateFlowNavHost()
+        }
+    }
+}
