afd44bf4f67e3718544a281fd294680ef4c5e867
Hamid Marzbani <hamidreza.marzbani@nexttoptech.org>
2026-01-24 15:10:54 +0300

    feat: Add navigation between property list and detail screens

--- feature/property/src/main/java/io/github/m4rzb4ni/feature_property/navigation/PropertyNavGraph.kt
+++ feature/property/src/main/java/io/github/m4rzb4ni/feature_property/navigation/PropertyNavGraph.kt
@@ -2,7 +2,11 @@ package io.github.m4rzb4ni.feature_property.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import io.github.m4rzb4ni.feature_property.ui.property_list.PropertyListScreen
+import io.github.m4rzb4ni.feature_property.ui.propety_detail.PropertyDetailScreen
 
 sealed class PropertyDestinations(val route: String) {
     object List : PropertyDestinations("property_list")
@@ -13,6 +17,18 @@ sealed class PropertyDestinations(val route: String) {
 
 fun NavGraphBuilder.propertyGraph(navController: NavHostController) {
     composable(PropertyDestinations.List.route) {
-
+        PropertyListScreen(
+            onNavigateToDetails = { propertyId ->
+                navController.navigate(PropertyDestinations.Detail.createRoute(propertyId = propertyId))
+            }
+        )
+    }
+    composable(
+        route = PropertyDestinations.Detail.route,
+        arguments = listOf(
+            navArgument("propertyId") { type = NavType.StringType }
+        )
+    ) {
+        PropertyDetailScreen()
     }
 }
